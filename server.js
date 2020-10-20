8b78e7ca502003b9f4310e7517d11a0dba7cfa98
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-10-20 16:23:13 -0400

    updated server.js with cors

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const { PORT = 4000, NODE_ENV = "development" } = process.env;
 
 //CORS
 const cors = require("cors");
-const corsOptions = require("./configs/cors.js");
+// const corsOptions = require("./configs/cors.js");
 
 // Add the middleware code needed to accept incoming data and add it to req.body
-NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
+// NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
