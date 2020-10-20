f60692a5d82883cb93d5b803bc9501fd3053e59d
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-10-20 08:35:37 -0400

    added cors middleware

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const logger = require('morgan')
 const app = express()
-const PORT = process.env.PORT
+// const PORT = process.env.PORT
+const { PORT = 4000, NODE_ENV = "development" } = process.env;
+
+//CORS
+const cors = require("cors");
+const corsOptions = require("./configs/cors.js");
 
 // Add the middleware code needed to accept incoming data and add it to req.body
+NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
 app.use(logger('dev'));
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
