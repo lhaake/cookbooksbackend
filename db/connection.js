a2d72197cc2577a2c06f04d1e00869c215cb30ab
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-10-20 00:04:40 -0400

    updates

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const mongoURI = process.env.mongoURI
+const MONGODBURI = process.env.MONGODBURI
 
 // mongoose.connect('mongodb://localhost/cookbooks', { useUnifiedTopology: true, useNewUrlParser: true })
 
@@ -14,7 +14,7 @@ const config = {
 }
 
 //CONNECT TO THE DATABASE
-mongoose.connect(mongoURI, config)
+mongoose.connect(MONGODBURI, config)
 
 mongoose.Promise = Promise
 
@@ -23,7 +23,7 @@ const db = mongoose.connection
 
 // ADDITIONAL MESSAGE CONNECTIONS. EVENT LISTENER 
 db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', mongoURI));
+db.on('connected', () => console.log('mongo connected: ', MONGODBURI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 
